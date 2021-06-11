c6f6313af9da28cdb6175d244900992c12521237
fukiran <macdorphoto@yahoo.com>
2021-06-11 17:17:39 +0100

    spring initialized

---
+++ dominion_backend/src/test/java/com/dominion/dominion_backend/DominionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominion.dominion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
